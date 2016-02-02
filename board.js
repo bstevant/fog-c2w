d535170c94781775bb7ace93e5b8f2b617970713
vagrant <vagrant@ubuntu-14.04-amd64-vbox>
2016-02-02 11:30:53 +0000

    Board microservice functional

--- board.js
+++ board.js
@@ -1,8 +1,50 @@
 var express = require('express');
 var app = express();
+var Redis = require("ioredis"),
+    client = new Redis();
 
+
+//////// Board class definition 
+function Board(re) { 
+    this.re = re;
+};
+Board.prototype.post = function (txt) {
+    var self = this;
+    this.re.incr("next_post_id").then(function (pid){
+        self.re.zadd("posts",pid,"#" + pid + " " + txt);
+    });
+};
+Board.prototype.get = function (last_idx,http_result) {
+    this.re.zrange("posts", last_idx, -1).then(function (replies){
+//        replies.forEach(function (reply, i){
+//            console.log("POST: " + reply);
+//        });
+        http_result.send(JSON.stringify(replies));
+    });
+};
+Board.prototype.erase = function () {
+    this.re.del("posts");
+};
+
+var board = new Board(client);
+
+/////// Web-service definition
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('This is the board.');
+});
+
+app.get('/post', function (req, res) {
+    board.post(req.query.p);
+    res.send('OK');
+});
+
+app.get('/get', function (req, res) {
+    board.get(0,res);
+});
+
+app.get('/erase', function (req, res) {
+    board.erase();
+    res.send('OK');
 });
 
 var server = app.listen(3000, function () {
@@ -11,5 +53,9 @@ var server = app.listen(3000, function () {
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+
+  client.on("error", function (err) {
+      console.log("Error " + err);
+  });
 
 });
