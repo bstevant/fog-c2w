91b61de2e9e3ebfa45cb1bb6563f3e5d5cb9cba9
Bruno STEVANT <bruno.stevant@telecom-bretagne.eu>
2016-07-04 14:39:19 +0200

    Fix startup of consul

--- start_consul_agent.sh
+++ start_consul_agent.sh
@@ -2,7 +2,8 @@
 
 /etc/init.d/dnsmasq start
 echo "nameserver 127.0.0.1" > /etc/resolv.conf
-tmux new -d /usr/bin/consul agent -data-dir /tmp/consul 
+IPADDR=`ip addr show eth1 | grep "scope global" | cut -d " " -f 6 | cut -d "/" -f 1`
+tmux new -d /usr/bin/consul agent -data-dir /tmp/consul -advertise $IPADDR
 
 if [ "$1" != "" ] ; then
     sleep 2
--- start_consul_server.sh
+++ start_consul_server.sh
@@ -6,7 +6,8 @@ if [ "$2" = "y" ] ; then
 fi
 
 /etc/init.d/dnsmasq start
-echo "nameserver 127.0.0.1" > /etc/resolv.conf 
-tmux new -d /usr/bin/consul agent -server -data-dir /tmp/consul -config-dir /etc/consul.d $BOOTSTRAP_OPT
+echo "nameserver 127.0.0.1" > /etc/resolv.conf
+IPADDR=`ip addr show eth1 | grep "scope global" | cut -d " " -f 6 | cut -d "/" -f 1`
+tmux new -d /usr/bin/consul agent -server -data-dir /tmp/consul -config-dir /etc/consul.d -advertise $IPADDR $BOOTSTRAP_OPT
 sleep 2
 /usr/bin/consul join $1
